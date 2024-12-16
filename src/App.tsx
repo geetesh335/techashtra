e273696511e197f0d859fcae7e9dc87435082e82
Geetesh Sahu <geeteshsahu335@gmail.com>
2024-12-16 19:39:25 +0530

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Body from './components/body/Body'
+import About from './components/about/About';
+import Footer from './components/footer/footer';
+import QualityCards from './components/qualityCards/QualityCard';
+
+
+function App() {
+  return (
+    <div className="App" >
+    <Body />
+     <About />
+    <QualityCards />
+   <Footer />
+    </div>
+  );
+}
+
+export default App;
